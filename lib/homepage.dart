769f85e5a7ef298808466f8ec1d8c21f38e1cb36
MaryAntonietteVincent <asenzmaryanto@gmail.com>
2023-12-01 21:44:32 +0700

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:instodrum/home/home.dart';
+import 'package:instodrum/pages/search.dart';
+import 'package:instodrum/home/reels.dart';
+import 'package:instodrum/pages/account.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void _navigateBottomNavBar(int Index) {
+    setState(() {
+      _selectedIndex = Index;
+    });
+  }
+
+//buat halaman masing-masing
+  final List<Widget> _children = [
+    UserHome(),
+    Search(),
+    UserReels(),
+    UserAccount(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //kerangka
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomNavBar,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_call), label: 'reels'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:instodrum/SplashScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyA3Uozfrlm2uPV3lAQgl9laYx6dTnuDOQE",
+          appId: "1:918914014784:android:49154aa93908fef48b49a5",
+          messagingSenderId: "918914014784",
+          projectId: "instodrum"));
+  runApp(
+    MyApp(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instodrum',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  final Widget? child;
+
+  MyApp({this.child});
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_MyAppState>()!.restartApp();
+  }
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Key key = UniqueKey();
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(
+      key: key,
+      child: widget.child!,
+    );
+  }
+}
